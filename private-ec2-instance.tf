5edc23f0ec489778de6b2f00211f86259f4966dd
MUKIT22 <mukitmuntasir@gmail.com>
2023-09-28 03:35:00 +0600

    completed

--- private-ec2-instance.tf
+++ private-ec2-instance.tf
@@ -15,20 +15,34 @@ module "ec2_private" {
   vpc_security_group_ids = [module.private_sg.security_group_id]
   user_data = <<-EOT
                 #!/bin/bash
-                sudo apt-get update
-                sudo apt-get install ca-certificates curl gnupg
+                sudo apt-get install ca-certificates curl gnupg -y
                 sudo install -m 0755 -d /etc/apt/keyrings
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
                 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
                 # Add the repository to Apt sources:
                 echo \
-                  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-                  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-                  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt-get update
+                "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update -y
 
-                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
+                # Define the GitHub repo URL and the project directory name
+                REPO_URL="https://github.com/MUKIT22/vprofile-project"
+
+                # Clone the repo using git clone command
+                sudo git clone $REPO_URL
+
+                # Change directory to the project directory using cd com
+
+                cd vprofile-project
+                sudo git checkout containers
+
+                # Run docker compose up using docker-compose command
+                sudo docker compose up
                 
                 EOT
 
