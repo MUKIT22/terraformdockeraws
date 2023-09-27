9cfeaf1a48488f57746f8144384657925fbb1032
MUKIT22 <mukitmuntasir@gmail.com>
2023-09-28 02:57:38 +0600

    further changes

--- private-ec2-instance.tf
+++ private-ec2-instance.tf
@@ -9,14 +9,27 @@ module "ec2_private" {
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   #monitoring             = true
- subnet_id = module.vpc.public_subnets[count.index]
+ subnet_id = module.vpc.private_subnets[count.index]
 
 
   vpc_security_group_ids = [module.private_sg.security_group_id]
   user_data = <<-EOT
                 #!/bin/bash
-                cd /home/ubuntu
-                touch xabc.txt  
+                sudo apt-get update
+                sudo apt-get install ca-certificates curl gnupg
+                sudo install -m 0755 -d /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+                # Add the repository to Apt sources:
+                echo \
+                  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+                  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update
+
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                
                 EOT
 
 }
