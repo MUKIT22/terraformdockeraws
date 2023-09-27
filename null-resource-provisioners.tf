c8ad54c45d83c14dfdf126a4fe1ac0181d2485fc
MUKIT22 <mukitmuntasir@gmail.com>
2023-09-27 17:55:17 +0600

    further changes

--- null-resource-provisioners.tf
+++ null-resource-provisioners.tf
@@ -1,24 +1,41 @@
-# Create a Null Resource and Provisioners
+
+
+ # Create a Null Resource and Provisioners
 resource "null_resource" "name" {
   depends_on = [module.ec2_public]
-  # Connection Block for Provisioners to connect to EC2 Instance
-  connection {
-    type     = "ssh"
-    host     = aws_eip.public_instance_eip.public_ip    
-    user     = "ubuntu"
-    password = ""
-    private_key = file("private-key/219.pem")
-  }  
 
-## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
+  # Connection Block for Provisioners to connect to Bastion Host
+  connection {
+    type        = "ssh"
+    host        = aws_eip.public_instance_eip.public_ip
+    user        = "ubuntu"
+    private_key = file("private-key/219.pem")
+  }
+
+  # File Provisioner: Copies the terraform-key.pem file to /tmp/219.pem on Bastion Host
   provisioner "file" {
     source      = "private-key/219.pem"
     destination = "/tmp/219.pem"
   }
-## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
+
+  # Remote Exec Provisioner: Using remote-exec provisioner, fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
       "sudo chmod 400 /tmp/219.pem"
     ]
   }
+
+  # Install Docker and Docker Compose on the private EC2 instances via Bastion Host
+  provisioner "remote-exec" {
+    when    = create
+    inline = [
+      # SSH into the first private instance via Bastion Host and execute Docker installation commands
+      "sudo ssh -o 'StrictHostKeyChecking no' -i /tmp/219.pem ubuntu@${module.ec2_private[0].private_ip} 'sudo touch rtyu.txt'",
+      
+      
+      # Repeat the same commands for the second private instance
+      "sudo ssh -o 'StrictHostKeyChecking no' -i /tmp/219.pem ubuntu@${module.ec2_private[1].private_ip} 'sudo touch rtyu.txt'"
+    ]
+  }
 }
+
--- private-ec2-instance.tf
+++ private-ec2-instance.tf
@@ -13,23 +13,10 @@ module "ec2_private" {
 
 
   vpc_security_group_ids = [module.private_sg.security_group_id]
-   user_data = <<-EOF
+  user_data = <<-EOT
                 #!/bin/bash
-                # Add Docker's official GPG key:
-                sudo apt-get update
-                sudo apt-get install ca-certificates curl gnupg
-                sudo install -m 0755 -d /etc/apt/keyrings
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-                sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-                # Add the repository to Apt sources:
-                echo \
-                  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-                  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-                  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt-get update
-
-                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                EOF
+                cd /home/ubuntu
+                touch xabc.txt  
+                EOT
 
 }
