2e21b4de9c58a204226bbfbd1a5f1562ede25f76
mukit22 <mukitmuntasir@gmail.com>
2023-10-05 03:58:55 +0600

    Untracking files

--- ec2instance.auto.tfvars
+++
@@ -1,3 +0,0 @@
-# EC2 Instance Variables
-instance_type = "t2.small"
-instance_keypair = "310"
--- elasticip.tf
+++
@@ -1,9 +0,0 @@
-# Create Elastic IP for Bastion Host
-# Resource - depends_on Meta-Argument
-resource "aws_eip" "public_instance_eip" {
-  depends_on = [ module.ec2_public, module.vpc ]
-  instance = module.ec2_public[0].id
-  domain   = "vpc"
-
-## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
-}
